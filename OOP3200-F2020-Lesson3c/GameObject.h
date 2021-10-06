f136ed3a5946405071f97e6d2944de2ec8b7d899
Katherine Bellman <katherine.bellman@dcmail.ca>
2021-10-06 15:02:18 -0400

    finished defining GameObject class and altered main.cpp for its use

---
+++ OOP3200-F2020-Lesson3c/GameObject.h
@@ -0,0 +1,36 @@
+#pragma once
+#ifndef  _GAME_OBJECT_
+#define  _GAME_OBJECT_
+#include "Vector2D.h"
+
+class GameObject
+{
+public:
+	//CONSTRUCTORS
+	GameObject();
+	GameObject(int id, float x, float y);
+	GameObject(int id, const Vector2D& position);
+
+	//RULE OF THREE
+	~GameObject(); //Destructor
+	GameObject(const GameObject& other_object); //Copy Constructor
+	GameObject& operator=(const GameObject& other_object); //Assignment Operator
+
+	//Accessors
+	Vector2D GetPosition() const;
+	int GetID() const;
+
+	//Mutators
+	void SetPosition(float x, float y);
+	void SetPosition(const Vector2D& new_position);
+	void SetID(int id);
+
+	//Utility Functions
+	std::string ToString() const;
+
+private:
+	Vector2D m_position;
+	int m_id;
+};
+#endif/* defined (_GAME_OBJECT_) */
+

46476cff43268b2ce4cd06249635836525832139
Katherine Bellman <katherine.bellman@dcmail.ca>
2021-10-06 18:42:27 -0400

    added GameObject class

--- OOP3200-F2020-Lesson3c/main.cpp
+++ OOP3200-F2020-Lesson3c/main.cpp
@@ -16,31 +16,42 @@
 
 int main()
 {
-	GameObject	gameObject1;
+	
+	int id1;
+	std::cout << "Enter the first gameObject ID: ";
+	std::cin >> id1;
 	Vector2D point1;
     std::cout << "Enter the first point (x, y): ";
 	std::cin >>  point1;
 	std::cout << "\n--------------------------------------------------------------" << std::endl;
-	std::cout << "You Entered " << point1 << " for the first point" << std::endl;
+	std::cout << "You Entered " << point1 << " for the first gameObject" << std::endl;
 	std::cout << "--------------------------------------------------------------\n" << std::endl;
-	gameObject1.SetPosition(point1);
+	GameObject	gameObject1(id1,point1);
 
-	GameObject gameObject2;
+	
+	int id2;
+	std::cout << "Enter the second gameObject ID: ";
+	std::cin >> id2;
 	Vector2D point2;
 	std::cout << "Enter the second point (x, y): ";
 	std::cin >> point2;
 	std::cout << "\n--------------------------------------------------------------" << std::endl;
-	std::cout << "You Entered " << point2 << " for the second point" << std::endl;
+	std::cout << "You Entered " << point2 << " for the second gameObject" << std::endl;
 	std::cout << "--------------------------------------------------------------\n" << std::endl;
-	gameObject2.SetPosition(point2);
+	GameObject gameObject2(id2,point2);
 
 	std::cout << std::fixed << std::setprecision(3);
 	std::cout << "Magnitude of first gameObject is: " << gameObject1.GetPosition().GetMagnitude() << std::endl;
 	std::cout << "Magnitude of second gameObject is: " << gameObject2.GetPosition().GetMagnitude() << std::endl;
-	std::cout << "Distance between first point and second point is: " << Vector2D::Distance(gameObject1.GetPosition(), gameObject2.GetPosition()) << std::endl;
+	std::cout << "Distance between first gameObject to the second gameObject is: " << Vector2D::Distance(gameObject1.GetPosition(), gameObject2.GetPosition()) << std::endl;
 	std::cout << "--------------------------------------------------------------\n" << std::endl;
 
-	std::cout << "The angle (in degrees) from the first vector to the second vector is: " << Vector2D::SignedAngle(gameObject1.GetPosition(), gameObject2.GetPosition()) << std::endl;
-	
+	std::cout << "The angle (in degrees) from the first gameObject to the second gameObject is: " << Vector2D::SignedAngle(gameObject1.GetPosition(), gameObject2.GetPosition()) << std::endl;
+
+	std::cout << "First Game Object details: " << std::endl;
+	std::cout << gameObject1.ToString() << std::endl;
+
+	std::cout << "Second Game Object details: " << std::endl;
+	std::cout << gameObject2.ToString() << std::endl;
 }
 
